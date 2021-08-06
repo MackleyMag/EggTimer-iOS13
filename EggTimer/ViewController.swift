c18d9474d59c5cbae7f2c9b04c2f98c6cdc268a8
MackleyMag <mackleymagalhaess@hotmail.com>
2021-08-06 11:26:26 -0300

    Timer complete
    timer complete

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,29 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
     
-    var secondsRemaning = 60
+    var totalTime = 0
+    var secondsPassed = 0
     
     var timer = Timer()
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        
+        progressBar.progress = 0.0
+        secondsPassed = 0
         timer.invalidate()
         
+        
         let hardness = sender.currentTitle!
-        secondsRemaning = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        
+        titleLabel.text = "Doing some progress to \(hardness)..."
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer(){
-        if secondsRemaning > 0{
-            print("\(secondsRemaning) seconds.")
-            secondsRemaning -= 1
+        if secondsPassed < totalTime{
+            
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            secondsPassed += 1
+            
+            progressBar.progress = percentageProgress
         }else{
             timer.invalidate()
+            progressBar.progress = 1.0
             titleLabel.text = "Done!"
         }
     }
